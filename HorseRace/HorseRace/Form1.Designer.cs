28281181304a9949000beba867f26d1ee48bc292
Ahmet KUNDURACIOGLU <50757093+KunduraciogluAhmet@users.noreply.github.com>
2024-06-10 07:59:30 +0300

    Add files via upload
    A very simple C# project, 3 horses running....

---
+++ HorseRace/HorseRace/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace HorseRace
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Horse1 = new System.Windows.Forms.PictureBox();
+            this.Horse2 = new System.Windows.Forms.PictureBox();
+            this.Horse3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Horse1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Horse2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Horse3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(1, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(835, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(1, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(835, 23);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(1, 345);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(835, 23);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(832, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 410);
+            this.label4.TabIndex = 3;
+            // 
+            // Horse1
+            // 
+            this.Horse1.Image = ((System.Drawing.Image)(resources.GetObject("Horse1.Image")));
+            this.Horse1.Location = new System.Drawing.Point(4, 54);
+            this.Horse1.Name = "Horse1";
+            this.Horse1.Size = new System.Drawing.Size(100, 50);
+            this.Horse1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Horse1.TabIndex = 4;
+            this.Horse1.TabStop = false;
+            // 
+            // Horse2
+            // 
+            this.Horse2.Image = ((System.Drawing.Image)(resources.GetObject("Horse2.Image")));
+            this.Horse2.Location = new System.Drawing.Point(4, 187);
+            this.Horse2.Name = "Horse2";
+            this.Horse2.Size = new System.Drawing.Size(100, 50);
+            this.Horse2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Horse2.TabIndex = 5;
+            this.Horse2.TabStop = false;
+            // 
+            // Horse3
+            // 
+            this.Horse3.Image = ((System.Drawing.Image)(resources.GetObject("Horse3.Image")));
+            this.Horse3.Location = new System.Drawing.Point(4, 318);
+            this.Horse3.Name = "Horse3";
+            this.Horse3.Size = new System.Drawing.Size(100, 50);
+            this.Horse3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Horse3.TabIndex = 6;
+            this.Horse3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(404, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 37);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(907, 484);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Horse3);
+            this.Controls.Add(this.Horse2);
+            this.Controls.Add(this.Horse1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Horse1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Horse2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Horse3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox Horse1;
+        private System.Windows.Forms.PictureBox Horse2;
+        private System.Windows.Forms.PictureBox Horse3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ HorseRace/HorseRace/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HorseRace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Horse1.Left += rnd.Next(5, 15);
+            Horse2.Left += rnd.Next(5, 15);
+            Horse3.Left += rnd.Next(5, 15);
+
+            // Bitiş çizgisine ulaşıp ulaşmadıklarını kontrol edin
+            if (Horse1.Left >= label4.Left - Horse1.Width ||
+                Horse2.Left >= label4.Left - Horse2.Width ||
+                Horse3.Left >= label4.Left - Horse3.Width)
+            {
+                timer1.Stop();
+                string winner = "";
+                if (Horse1.Left >= label4.Left - Horse1.Width)
+                {
+                    winner = "Horse 1";
+                }
+                else if (Horse2.Left >= label4.Left - Horse2.Width)
+                {
+                    winner = "Horse 2";
+                }
+                else if (Horse3.Left >= label4.Left - Horse3.Width)
+                {
+                    winner = "Horse 3";
+                }
+                MessageBox.Show($"{winner} wins!");
+            }
+        }
+    }
+}
